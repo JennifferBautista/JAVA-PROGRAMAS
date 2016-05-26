e85deb5fdf914f9d7050532cbbfbebff7f7b0b68
Jenniffer Bautista <jenniffer9517@gmail.com>
2016-05-25 22:42:22 -0500

    Tarea CADENA deleteChar

--- Cadena/src/cadena/cadena.java
+++ Cadena/src/cadena/cadena.java
@@ -21,10 +21,5 @@ public class cadena {
 	public void set_cadena(String cad){
 		this.cadena = cad;
 	}
-	public static String deleteChar(int n, String cad){
-		String cad2 = cad.substring(0, n);
-		String cad3 = cad.substring(n + 1);
-		String cad4 = cad2 + cad3;
-		return cad4;
-	}
+	
 }
