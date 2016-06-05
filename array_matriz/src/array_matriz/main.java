6f78c5d7be8189575ee35f70eb7ac039ba5002f4
Jenniffer Bautista <jenniffer9517@gmail.com>
2016-06-04 23:00:21 -0500

    Matriz de 8x8 ordenada

--- array_matriz/src/array_matriz/main.java
+++ array_matriz/src/array_matriz/main.java
@@ -4,11 +4,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int aux[][]={{1,3,6,7,4,5,2,8},{1,3,6,7,4,5,2,8}};
+		int aux[][]={{1,2,3,4,5,6,7,8},{9,10,11,12,13,14,15,16}};
 		matriz mat = new matriz(aux);
         mat.getMax(aux);
         mat.getMin(aux);
         mat.getpromedio(aux);
+        mat.ordenar_Asc(aux);
+        mat.ordenar_Desc(aux);
 	}
-
 }
--- array_matriz/src/array_matriz/matriz.java
+++ array_matriz/src/array_matriz/matriz.java
@@ -3,15 +3,15 @@ package array_matriz;
 
 public class matriz {
 
-     public matriz (int[][] aux){
-		int ma [] []= new int [8][8]; 
+     public matriz (int arr[][] ){
+		int mat [] []= new int [8][8]; 
 	}
 
  	public void getMax(int[][]arr){
  		int max = arr[0][0];
  		 for(int x=0; x<arr.length; x++)
  		 {
- 			 for(int y=0; y<arr.length; y++)
+ 			 for(int y=0; y<arr[x].length; y++)
  		 	if (arr[x][y]>max)
  	          max=arr[x][y];
  		 }
@@ -22,7 +22,7 @@ public class matriz {
  		int min = arr[0][0];
  		 for(int x=0; x<arr.length; x++)
  		 {
- 			for(int y=0; y<arr.length; y++)
+ 			for(int y=0; y<arr[x].length; y++)
  		 	if (arr[x][y]<min)
  	          min=arr[x][y];
  		 }
@@ -31,15 +31,58 @@ public class matriz {
  	}
  	public void getpromedio(int arr[][])
  	{
- 	    float temp=0, suma=0;
+ 	    float  suma=0;
  	 
  	  for (int x=0; x<arr.length; x++)
  	  {
- 		  for (int y=0; y<arr.length; y++)
- 	     suma+=arr[x][y];
- 	        temp++;
+ 		  for (int y=0; y<arr[x].length; y++)
+ 	     suma=suma+arr[x][y];
  	  }
- 	 System.out.println("El promedio es: "+(suma/temp));
+ 	 System.out.println("El promedio es: "+(suma/16));
  	  
 }
+ 	public void ordenar_Asc(int arr[][]){
+ 		
+		for( int i=0; i < arr.length; i++){
+			for(int j=0;j< arr[i].length; j++){
+				for(int x=0; x < arr.length; x++){
+					for(int y=0; y <arr[x].length; y++){
+						if(arr[i][j] < arr[x][y]){
+							int t = arr[i][j];
+							arr[i][j] = arr[x][y];
+							arr[x][y] = t;}
+						}
+					}
+				}
+		}
+		System.out.println("**Matriz ASC**");
+
+		for (int i=0; i < arr.length; i++){//imprime la matriz ascendentemente
+			for(int j=0;j< arr[i].length; j++){
+				System.out.print("  "+arr[i][j]);
+			}
+			System.out.println();}
+		}
+ 	public void ordenar_Desc(int arr[][]){
+ 		
+		for( int i=0; i < arr.length; i++){
+			for(int j=0;j< arr[i].length; j++){
+				for(int x=0; x < arr.length; x++){
+					for(int y=0; y <arr[x].length; y++){
+						if(arr[i][j] > arr[x][y]){
+							int t = arr[i][j];
+							arr[i][j] = arr[x][y];
+							arr[x][y] = t;}
+						}
+					}
+				}
+		}
+		System.out.println("**Matriz DESC**");
+
+		for (int i=0; i < arr.length; i++){//imprime la matriz descendentemente
+			for(int j=0;j< arr[i].length; j++){
+				System.out.print("  "+arr[i][j]);
+			}
+			System.out.println();}
+		}
 }
