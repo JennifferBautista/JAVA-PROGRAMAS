d3c7d0aed06b856f4ba8163b888b71bfda26ee1b
Jenniffer Bautista <jenniffer9517@gmail.com>
2016-07-02 12:47:13 -0500

    Ficheros-Lectura de archivo.log

---
+++ LecturaProtecto/src/lectura_ip.java
@@ -0,0 +1,4 @@
+import java.io.IOException;
+public interface lectura_ip {
+	void lectura() throws IOException; 
+}
---
+++ LecturaProtecto/src/main.java
@@ -0,0 +1,12 @@
+
+import java.io.IOException;
+public class main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		archivo_ip leer = new archivo_ip();
+		leer.lectura();
+	}
+
+}
